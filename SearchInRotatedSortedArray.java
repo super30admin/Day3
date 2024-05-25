169aee8007d83962e50a53de141da5023e85d986
Tanmay-Soni <73430591+Tanmay-Soni@users.noreply.github.com>
2024-05-25 17:26:36 -0400

    Create SearchInRotatedSortedArray.java

---
+++ SearchInRotatedSortedArray.java
@@ -0,0 +1,36 @@
+// Time complexity: O(log(n))
+// Space complexity: O(1)
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        if (nums.length == 0){
+            return -1;
+        }
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(nums[mid] >= nums[low]){
+                if(target >= nums[low] && target < nums[mid]){
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if(target <= nums[high] & target > nums[mid]){
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
