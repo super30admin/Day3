c0ce5343ddeafcac38f9034734cc91133dd10344
pradnyabhukan <bhukanpradnya5@gmail.com>
2024-06-20 14:46:46 -0400

    wip: Binary search on rotated sorted array

---
+++ BinarySeachOnRotatedSortedArray.java
@@ -0,0 +1,29 @@
+class BinarySeachOnRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+
+        while(left <= right) {
+            mid = left + (right - left)/2;
+            //base case
+            if(nums[mid] == target) return mid;
+
+            //check if left side is sorted
+            if(nums[left] <= nums[mid]) {
+                if(nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else { //right is sorted
+                if(nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
