6832774d7e977e466d73b7e56800b19c5621e5af
pradnyabhukan <bhukanpradnya5@gmail.com>
2024-06-21 11:07:42 -0400

    optimized 2D search matrix; Infinte sorted array

--- InfinitSortedArray.java
+++ InfinitSortedArray.java
@@ -6,8 +6,19 @@
  */
 
 class InfinteSortedArray {
-    private int binarySearch() {}
     public int search(ArrayReader reader, int target) {
-        
+        int low = 0;
+        int high = 1;
+        while(reader.get(high) <= target) {
+            low = high;
+            high = 2*high;
+        }
+        while(low <= high) {
+            int mid = low + (high-low)/2;
+            if(reader.get(mid) == target) return mid;
+            else if(target < reader.get(mid)) high = mid - 1;
+            else low = mid + 1;
+        }
+        return -1;
     }
 }
--- SearchMatrix.java
+++ SearchMatrix.java
@@ -1,38 +1,24 @@
-//time complexity O(logn + logm) not sure
+//time complexity O(log(mn)) not sure
 //space compexity O(1)
+//Did this code successfully run on Leetcode : yes
 
 class Solution {
-    public boolean binarySort(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        int mid;
-        while(left <= right) {
-            mid = left + (right - left)/2;
-            if(target == nums[mid]) return true;
-            else if(target < nums[mid]) {
-                right = mid - 1;
-            } else if (target > nums[mid]) {
-                left = mid + 1;
-            }
-        }
-        return false;
-    }
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix[0].length;
-        int n = matrix.length;
-        if(m == 0 || n== 0) return false;
-        int left = 0;
-        int right = n-1;
-        int mid;
-        while(left<=right) {
-            mid = left + (right - left)/2;
-            if(matrix[mid][0] == target || matrix[mid][m-1]==target) return true;
-            if(matrix[mid][0] < target && matrix[mid][m-1] > target) {
-                return binarySort(matrix[mid], target);
-            } else if(matrix[mid][0] > target) {
-                right = mid - 1;
-            } else if(matrix[mid][0] < target) {
-                left = mid + 1;
+        int n = matrix[0].length;
+        int m = matrix.length;
+        System.out.println(n + " size " + m);
+        int low = 0;
+        int high = m*n - 1;
+
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            int row = mid/n;
+            int col = mid%n;
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         return false;
