af8c3176307d95be5b53c0e603c625aa80f76b30
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2024-06-21 19:31:19 -0400

    BS1

---
+++ rotated_search_array.py
@@ -0,0 +1,28 @@
+"""
+Time Compexity: O(logn)
+Sace Complexity: O(log n)
+"""
+
+
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+
+        while(low <= high):
+            mid = (low+high) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if nums[mid] >= target and nums[low] <= target:
+                    high = mid-1
+                else:
+                    low = mid+1
+            else:
+                if nums[mid] <= target and nums[high] >= target:
+                    low = mid +1
+                else:
+                    high = mid - 1
+        return -1
