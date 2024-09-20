690bb143e20690dadeda6e1c233407d21b01f3b1
SriVinayA <srivinay.appari@gmail.com>
2024-09-20 04:06:16 -0700

    problem 3

--- problem3.py
+++ problem3.py
@@ -28,5 +28,21 @@ Example 2:
 
 class Solution:
     def search(self, reader: 'ArrayReader', target: int) -> int:
+        low = 0
+        high = 1
         
+        while (reader.get(high)<target):
+            low = high
+            high = high*2
+
+        while low<=high:
+            mid = low + (high-low)//2
+
+            if reader.get(mid) == target:
+                return mid
+            elif reader.get(mid) > target:
+                high = mid-1
+            else:
+                low = mid+1
+
         return -1
