e31345b5098364c8b782682ab1d871f8d5020685
Malav Soni <malav@live.com>
2024-10-24 22:27:03 +0530

    2nd way implemented

--- search-in-2d-array.js
+++ search-in-2d-array.js
@@ -26,13 +26,58 @@ var searchMatrix = function (matrix, target) {
   return false;
 };
 
-console.log(
-  searchMatrix(
-    [
-      [1, 3, 5, 7],
-      [10, 11, 16, 20],
-      [23, 30, 34, 60],
-    ],
-    3
-  )
+var searchMatrixV2 = function (matrix, target) {
+  let row = find_row_index(matrix, target);
+  console.log("Row " + row);
+  if (row === -1) return false;
+  let col = find_col_index(matrix[row], target);
+  console.log("Col " + row);
+  if (col === -1) return false;
+  return matrix[row][col] == target;
+};
+
+var find_row_index = function (array, target) {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+    let high = array[mid][array[mid].length - 1];
+    let low = array[mid][0];
+    if (low <= target && target <= high) {
+      return mid;
+    } else if (high >= target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return -1;
+};
+
+var find_col_index = function (array, target) {
+  let left = 0;
+  let right = array.length;
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+    let element = array[mid];
+    if (element == target) {
+      return mid;
+    } else if (element >= target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return -1;
+};
+
+let result = searchMatrixV2(
+  [
+    [1, 3, 5, 7],
+    [10, 11, 16, 20],
+    [23, 30, 34, 60],
+  ],
+  3
 );
+console.log("Result " + result);
