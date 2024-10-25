8eb9652884a6845c4025fa4a7907e16e00f837f7
LEGEND0WSKI <siddhesh.koli32@gmail.com>
2024-10-24 22:03:49 -0500

    2/3 done

---
+++ infSortedArray.py
@@ -0,0 +1,27 @@
+# // Time Complexity :
+# // Space Complexity :
+# // Did this code successfully run on Leetcode :
+# // Any problem you faced while coding this :
+
+
+# // Your code here along with comments explaining your approach in three sentences only
+
+
+class ArrayReader:
+    def get(self, index: int) -> int:
+
+
+class Solution:
+    def search(self, reader: 'ArrayReader', target: int) -> int:
+        l = 0
+        h = 1
+        while h <= index:
+            l = h
+            h = h * 2
+        
+        while 
+
+
+
+
+# y = Solution()
