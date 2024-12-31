472a2c1acfaecf76e1f22329eaf69c467e76a086
Chetan Khilare <chetan.kh23@gmail.com>
2024-12-31 10:16:29 -0500

    Added time complexity and space complexity

--- SearchInUnknownArraySize.java
+++ SearchInUnknownArraySize.java
@@ -1,3 +1,10 @@
+/**
+ * Time Complexity: O(log n)
+ * Space Complexity: O(1)
+ * Did this code successfully run on Leetcode: Yes
+ * Any problem you faced while coding this: No
+ */
+
 public class SearchInUnknownArraySize {
   public int search(ArrayReader reader, int target) {
     int low = 0;
