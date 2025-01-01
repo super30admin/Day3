b6911bd5ef4e84ac92da6cfba23bbe70d9448e15
padmaanaokar <anaokar.p@northeastern.edu>
2024-12-31 18:36:56 -0600

    Last file uploaded

--- searchInUnknownSizeRSA.java
+++ searchInUnknownSizeRSA.java
@@ -1,7 +1,34 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity :O(log n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
 
+class Solution {
+    public int search(ArrayReader reader, int target) {
+        int low = 0;
+        int high = 1;
 
-// Your code here along with comments explaining your approach
+        while(reader.get(high) < target){
+            low = high;
+            high = high * 2;
+        }
+
+        if(reader.get(high) == target) return high;
+
+        while(low <= high){
+
+            int mid = low + (high - low)/2;
+
+            if(reader.get(mid) == target){
+                return mid;
+            }else if(reader.get(mid) > target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+        
+    }
+}
