2c487bda1744afb041efbc8782656e8043ab1098
sahildps <sahildshetty@gmail.com>
2025-01-01 15:32:24 -0700

    added comments

--- Problem-2.cpp
+++ Problem-2.cpp
@@ -1,3 +1,8 @@
+// author : Sahil D Shetty
+//  Time Complexity : O(log N)
+//  Space Complexity : O(1)
+//  Did this code successfully run on Leetcode : Yes
+//  Any problem you faced while coding this : No
 class Solution
 {
 public:
