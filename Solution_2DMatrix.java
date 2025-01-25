5e220872c77d3ae8585cb4129f88b7a7e3324c4c
AvaniVaidya <avanivaidya1997@gmail.com>
2025-01-24 18:48:33 -0800

    Binary Search Exercise 1 in java

---
+++ Solution_2DMatrix.java
@@ -0,0 +1,26 @@
+//TC: O(logn)
+//SC: O(1)
+public class Solution_2DMatrix {
+    public int search(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target)
+                return mid;
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target < nums[mid])
+                    high = mid-1;
+                else
+                    low = mid + 1;
+            } else {
+                if(nums[mid] < target && target <= nums[high])
+                    low = mid+1;
+                else
+                    high = mid-1;
+            }
+        }
+        return -1;
+    }
+}
