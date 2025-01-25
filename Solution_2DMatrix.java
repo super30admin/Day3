28f6e0545b6945aa8e6dfeac83044551e7b8bf5d
AvaniVaidya <avanivaidya1997@gmail.com>
2025-01-24 18:51:07 -0800

    Corrected code for 2D matrix

--- Solution_2DMatrix.java
+++ Solution_2DMatrix.java
@@ -1,26 +1,35 @@
-//TC: O(logn)
+//TC: O(logm + logn)
 //SC: O(1)
 public class Solution_2DMatrix {
-    public int search(int[] nums, int target) {
+    public boolean searchMatrix(int[][] matrix, int target) {
 
+        int rows = matrix.length;
+        int cols = matrix[0].length;
         int low = 0;
-        int high = nums.length - 1;
+        int high = rows-1;
+        int mid = 0;
         while(low <= high){
-            int mid = low + (high - low)/2;
-            if(nums[mid] == target)
-                return mid;
-            if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && target < nums[mid])
-                    high = mid-1;
-                else
-                    low = mid + 1;
-            } else {
-                if(nums[mid] < target && target <= nums[high])
-                    low = mid+1;
-                else
-                    high = mid-1;
+            mid = low + (high - low)/2;
+            if(matrix[mid][0] <= target && target <= matrix[mid][cols-1]){
+                break;
             }
+            if(target < matrix[mid][0])
+                high = mid - 1;
+            else
+                low = mid + 1;
         }
-        return -1;
+        low = 0;
+        high = cols - 1;
+        int r = mid;
+        while(low <= high){
+            mid = low + (high - low)/2;
+            if(matrix[r][mid] == target)
+                return true;
+            if(target < matrix[r][mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return false;
     }
 }
