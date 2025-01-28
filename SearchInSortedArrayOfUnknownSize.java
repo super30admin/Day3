9fc1d6ffaa66f5b6b0f52fed181503d8a5506057
uma lakkaraju <u0l006r@walmart.com>
2025-01-27 18:35:22 -0800

    LC74-SearchMatrixWithSortedRowsAndCols

--- SearchInSortedArrayOfUnknownSize.java
+++ SearchInSortedArrayOfUnknownSize.java
@@ -1,2 +1,23 @@
-public class SearchInSortedArrayOfUnknownSize {
+class SearchInSortedArrayOfUnknownSize {
+    public int search(ArrayReader reader, int target) {
+        int low=0;
+        int high=1;
+        while(reader.get(high)<target){
+            low=high;
+            high=2*high;
+        }
+        while(low <=high){
+            int mid=low+(high-low)/2;
+            if(reader.get(mid)==target){
+                return mid;
+            }
+            if(reader.get(mid)>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+
+        }
+        return -1;
+    }
 }
