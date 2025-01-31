cb165e0d4c1b77a80ef2626cd98d3682a444ffd6
Viswanadh-Kavuri <visukavuri1996@gmail.com>
2025-01-31 04:10:26 -0600

    add solution for problem2, problem3

---
+++ Problem-3.java
@@ -0,0 +1,25 @@
+class SearchInASortedArrayOfUnknownSize {
+    public int search(ArrayReader reader, int target) {
+        int upperBound = 1;
+        int low = 0;
+        int mid;
+        while (reader.get(upperBound) <= target) {
+            upperBound = upperBound * 2;
+        }
+
+        while (low < upperBound) {
+            mid = low + (upperBound - low) / 2;
+            int currMid = reader.get(mid);
+            if (currMid == target)
+                return mid;
+            if (target > currMid) {
+                low = mid + 1;
+            } else
+                upperBound = mid - 1;
+        }
+        if (reader.get(upperBound) == target)
+            return upperBound;
+        return -1;
+
+    }
+}
